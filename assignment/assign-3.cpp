0dd0729373196beb94a3587c12a5cc0f3c4ff679
Rajatkrishna singh <rajatkrishna.singh10e27@gmail.com>
2026-09-22 20:39:10 +0530

    Create assign-3.cpp

---
+++ assignment/assign-3.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+class emp
+{
+private:
+    string name;
+    int no;
+    int salary;
+    string dept;
+
+public:
+    void input()
+    {
+        cout << "Name of Employee:";
+        cin >> name;
+        cout << "Phone Number:";
+        cin >> no;
+        cout << "Salary:";
+        cin >> salary;
+        cout << "Department:";
+        cin >> dept;
+        cout << "-----------------------" << endl
+             << endl;
+    }
+
+    void display()
+    {
+
+        cout << "Name of Employee:" << name << endl;
+        cout << "Phone Number:" << no << endl;
+        cout << "Salary:" << salary << endl;
+        cout << "Department:" << dept << endl;
+        cout << "-------------------------" << endl;
+    }
+};
+int main()
+{
+
+    emp e1;
+    emp e2;
+    emp e3;
+
+    e1.input();
+    e2.input();
+    e3.input();
+
+    cout << "-----Employee Details-----" << endl;
+    e1.display();
+    e2.display();
+    e3.display();
+    return 0;
+}
